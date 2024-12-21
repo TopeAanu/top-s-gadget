00513a20557a65ea4da019f8eef8b203b9202d70
TopeAanu <topefasasi99@gmail.com>
2024-12-21 23:36:05 +0100

    cupdate: handling loader & errors

--- src/components/Gadgets/AvailableGadgets.js
+++ src/components/Gadgets/AvailableGadgets.js
@@ -6,12 +6,18 @@ import classes from "./AvailableGadgets.module.css";
 
 const AvailableGadgets = () => {
   const [gadgets, setGadgets] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchGadgets = async () => {
       const response = await fetch(
         "https://order-gadgets-app-default-rtdb.firebaseio.com/gadgets.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
       const responseData = await response.json();
       const loadGadgets = [];
@@ -25,10 +31,30 @@ const AvailableGadgets = () => {
         });
       }
       setGadgets(loadGadgets);
+      setIsLoading(false);
     };
 
-    fetchGadgets();
+    fetchGadgets().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.GadgetsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.GadgetsError}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
 
   const gadgetsList = gadgets.map((gadget) => (
     <GadgetItem
