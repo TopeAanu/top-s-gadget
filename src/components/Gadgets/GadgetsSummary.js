f14c53bc88df7e28b2bfa02c91f1d230b7ba3093
TopeAanu <topefasasi99@gmail.com>
2025-01-03 23:42:09 +0100

    update

--- src/components/Gadgets/GadgetsSummary.js
+++ src/components/Gadgets/GadgetsSummary.js
@@ -3,9 +3,9 @@ import classes from './GadgetsSummary.module.css';
 const GadgetsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Quality & Affordable Gadgets!</h2>
+      <h2>Quality, Affordable Gadgets!</h2>
       <p>
-      Top’s Gadgets is here to serve you with high-quality gadgets and accessories at the best sale prices. We deal in iPhones, Samsung, Redmi, Tecno, laptops, tablets, Playstations, etc. We also offer first-hand repairs on gadgets. Patronize us today and get the best experience ever! 
+      Afro’s Gadgets is here to serve you with high-quality gadgets and accessories at the best sale prices. We deal in iPhone, Samsung, Redmi, Tecno, laptops, tablets, Playstations, etc. We also offer first-hand repairs on gadgets. Patronize us today and get the best experience ever! 
       </p>
       <h3>
       See available gadgets below👇.
