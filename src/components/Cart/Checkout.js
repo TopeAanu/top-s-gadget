61815c2ce7f06aa22b705f17ac70fa9e06d7ff42
TopeAanu <topefasasi99@gmail.com>
2025-01-04 16:18:25 +0100

    update

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,5 +1,4 @@
 import { useRef, useState } from 'react';
-
 import classes from './Checkout.module.css';
 
 const isEmpty = (value) => value.trim() === '';
@@ -56,6 +55,13 @@ const Checkout = (props) => {
     });
   };
 
+  const inputChangeHandler = (input) => {
+    setFormInputsValidity((prevState) => ({
+      ...prevState,
+      [input]: true, // Reset the validity for the specific input
+    }));
+  };
+
   const nameControlClasses = `${classes.control} ${
     formInputsValidity.name ? '' : classes.invalid
   }`;
@@ -73,24 +79,44 @@ const Checkout = (props) => {
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={nameControlClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' ref={nameInputRef} />
+        <input
+          type='text'
+          id='name'
+          ref={nameInputRef}
+          onChange={() => inputChangeHandler('name')}
+        />
         {!formInputsValidity.name && <p>Please enter a valid name!</p>}
       </div>
       <div className={streetControlClasses}>
         <label htmlFor='street'>Street</label>
-        <input type='text' id='street' ref={streetInputRef} />
+        <input
+          type='text'
+          id='street'
+          ref={streetInputRef}
+          onChange={() => inputChangeHandler('street')}
+        />
         {!formInputsValidity.street && <p>Please enter a valid street!</p>}
       </div>
       <div className={postalCodeControlClasses}>
         <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' ref={postalCodeInputRef} />
+        <input
+          type='text'
+          id='postal'
+          ref={postalCodeInputRef}
+          onChange={() => inputChangeHandler('postalCode')}
+        />
         {!formInputsValidity.postalCode && (
           <p>Please enter a valid postal code (5 characters long)!</p>
         )}
       </div>
       <div className={cityControlClasses}>
         <label htmlFor='city'>City</label>
-        <input type='text' id='city' ref={cityInputRef} />
+        <input
+          type='text'
+          id='city'
+          ref={cityInputRef}
+          onChange={() => inputChangeHandler('city')}
+        />
         {!formInputsValidity.city && <p>Please enter a valid city!</p>}
       </div>
       <div className={classes.actions}>
--- src/components/Gadgets/GadgetsSummary.js
+++ src/components/Gadgets/GadgetsSummary.js
@@ -5,7 +5,7 @@ const GadgetsSummary = () => {
     <section className={classes.summary}>
       <h2>Quality, Affordable Gadgets!</h2>
       <p>
-      Afro’s Gadgets is here to serve you with high-quality gadgets and accessories at the best sale prices. We deal in iPhone, Samsung, Redmi, Tecno, laptops, tablets, Playstations, etc. We also offer first-hand repairs on gadgets. Patronize us today and get the best experience ever! 
+      Top’s Gadgets is here to serve you with high-quality gadgets and accessories at the best sale prices. We deal in iPhone, Samsung, Redmi, Tecno, laptops, tablets, Playstations, etc. We also offer first-hand repairs on gadgets. Patronize us today and get the best experience ever! 
       </p>
       <h3>
       See available gadgets below👇.
