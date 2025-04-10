b759e7fe511f956194c5c16e3e249c1136031c50
TopeAanu <topefasasi99@gmail.com>
2025-04-10 15:00:50 +0100

    fixed firebase issue, update metadata, and favicon

--- src/components/Gadgets/GadgetsSummary.js
+++ src/components/Gadgets/GadgetsSummary.js
@@ -5,10 +5,10 @@ const GadgetsSummary = () => {
     <section className={classes.summary}>
       <h2>Quality, Affordable Gadgets!</h2>
       <p>
-      Top’s Gadgets is here to serve you with high-quality gadgets and accessories at the best sale prices. We deal in iPhone, Samsung, Redmi, Tecno, laptops, tablets, Playstations, etc. We also offer first-hand repairs on gadgets. Patronize us today and get the best experience ever! 
+      Top’s Gadgets is an online store that sells high-quality gadgets and accessories at the best sale prices. We deal in iPhone, Samsung, Redmi, Tecno, laptops, tablets, Playstations, etc. We also offer high-quality repair services on gadgets. 
       </p>
       <h3>
-      See available gadgets below👇.
+      Find available gadgets below👇.
       </h3>
     </section>
   );
